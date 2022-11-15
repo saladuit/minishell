8f8a955a1253270cf7a02e9f2fd557cb308406ff
Saladin Afoh <safoh@student.codam.nl>
2022-11-15 13:28:42 +0100

    Slowly going | instead of recursively waiting on all childs, put them in a list

--- src/executor.c
+++ src/executor.c
@@ -1,3 +1,4 @@
+#include "command_table.h"
 #include <executor.h>
 #include <message.h>
 #include <redir.h>
@@ -75,9 +76,6 @@ pid_t	execute_command(int32_t in_fd, int32_t out_fd, t_command *command, char **
 	signal(SIGINT, SIG_DFL);
 	if (!protected_dup2(in_fd, INPUT) || !protected_dup2(out_fd, OUTPUT))
 		exit(errno);
-	setup_redirects(command, &in_fd, &out_fd);
-	arguments = get_arguments(command);
-	command_path = get_cmd_path(envp, arguments[0]);
 	if (access(command_path, X_OK))
 		printf("%s: %s: %s\n", "Minishell", arguments[0], "Command not found");
 	execve(command_path, arguments, envp);
@@ -115,21 +113,57 @@ int32_t	pipe_commands(t_command_table *ct, char **envp)
 	return (pid);
 }
 
-int32_t	executor(t_list *ast, char **envp)
+int32_t execute_simple_command(t_command_table *ct, char **envp)
 {
-	t_command_table	*ct;
-	int32_t			status;
-	pid_t			pid;
+	t_command	cmd;
+	pit_t		pid;
+	char	*command_path;
+	char	**arguments;
+
+	cmnd = get_next_command(ct);
+	arguments = get_arguments(cmd);
+	if (is_builtin_command(arguments))
+		return (execute_builtin(arguments, envp));
+	command_path = get_cmd_path(envp, arguments[0]);
+	pid = fork();
+	if (pid != 0)
+		return (pid);
+	setup_redirects(command, &in_fd, &out_fd);
+}
+
+int32_t execute_pipeline(t_command_table *ct, char **envp)
+{
+	int32_t status;
+	t_list	*pid;
+	while (pid)
+	{
+		waitpid(*(int *)pid->content, &status, WUNTRACED);
+		pid = pid->next;
+	}
+	ft_lstclear(&pid, free);
+}
+
+int32_t	execute_command_table(t_command_table *ct, char **envp)
+{
+	if (ct->commands->next == NULL)
+		return (execute_simple_command(ct, envp));
+	return (execute_pipeline(ct, envp));
+}
+
+int32_t executor(t_list *ast, char **envp)
+{
+	t_command_table *ct;
+	int32_t status;
+
 
 	status = 0;
 	ct = get_next_command_table(&ast);
 	while (ct)
 	{
-		pid = pipe_commands(ct, envp);
-		waitpid(pid, &status, WUNTRACED);
-		wait(NULL);
+		status = execute_command_table(ct, envp);
 		free(ct);
+		ct = NULL;
 		ct = get_next_command_table(&ast);
 	}
-	return (WEXITSTATUS(status));
+	return (status); //should be removed from here
 }
