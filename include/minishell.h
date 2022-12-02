b33755a995a17d867c5cea11b7e4612170392266
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2022-12-02 18:51:18 +0100

    started on something

--- include/minishell.h
+++ include/minishell.h
@@ -22,6 +22,24 @@ typedef struct s_minishell
 
 }	t_minishell;
 
+typedef struct s_redir
+{
+	char	*filename;
+	t_type	type;
+}	t_redir;
+
+// Mini_Enums
+
+typedef enum e_type
+{
+	INPUT,
+	OUTPUT,
+	APPEND,
+	HEREDOC
+}	t_type;
+
+
+
 char	**dup_envp(char **envp);
 int32_t	init_handlers(void);
 int32_t	minishell(t_minishell *shell);
@@ -38,5 +56,49 @@ bool	is_double_quoted(char *str);
 bool	is_single_quote(int c);
 bool	is_single_quoted(char *str);
 bool	is_expand(int c);
+
+/**
+ *  @brief Converts tokens into an Abstract Syntax Tree.
+ *  Exits program on failure.
+ *
+ *  An Abstract Syntax Tree is a linked list of chained command tables.
+ *  Each command table is delimited by:
+ *  - ;
+ *  - &&
+ *  - ||
+ *  A command table contains a counter to keep track of the amount of commands
+ *  and a linked list of commands.
+ *
+ *  A command consists out of arguments and redirections and
+ *  a counter for the amount of arguments.
+ *
+ *  The redirections have their own structure containing a filename and type.
+ *
+ *  The Data Structures used are defined in command_table.h & command.h respectively.
+ *
+ *  The parser provides interfaces to better communicate with this data structure.
+ *  These are defined in the astapi.h
+ *  - Iterating to the next command table.
+ *  - Iterating to the next command.
+ *  - Getting the command arguments.
+ *  - Getting the next redirection struct.
+ *
+ *  @param ast		-	Empty linked list
+ *  @param tokens	-	The tokens to be placed incside the AST
+ *
+ *  @return 		- The constructed AST
+ */
+t_list	*parser(t_list *tokens);
+
+/*
+ * @brief contructs a redirection struct out of two tokens
+ * the first token will become the redirection type and the second
+ * will become the filname
+ *
+ * @param command	-	Linked list with tokens
+ * @return 			-	Redirection struct
+ */
+
+t_redir	*construct_redir(t_list **tokens);
 
 #endif
