2f1504c557687ecbea42a4015bf44ed7a01c2768
Duco Ritsema van Eck <ducorits@zonderstreepje.nl>
2023-01-12 15:58:15 +0100

    Exit something?

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,10 +1,10 @@
 #include <minishell.h>
 
-int ft_exit(char **arguments, t_minishell *shell)
+int	ft_exit(char **arguments, t_minishell *shell)
 {
 	(void)arguments;
 	(void)shell;
 	rl_clear_history();
-	exit(0);
+	exit(ft_atoi(arguments[1]));
 	return (0);
 }
