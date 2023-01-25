c01be3af40a5bca6482a6a534d4397cf2ba2488d
Saladin Afoh <safoh@student.codam.nl>
2023-01-25 19:09:41 +0100

    Doesn't accept codes outside of 0 and 255

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -2,11 +2,16 @@
 
 int	ft_exit(char **arguments, t_minishell *shell)
 {
+	int	return_code;
 	(void)arguments;
 	(void)shell;
 	rl_clear_history();
 	if (arguments[1])
-		exit(ft_atoi(arguments[1]));
+	{
+		return_code = ft_atoi(arguments[1]);
+		if (return_code < 0 || return_code > 255)
+			exit(return_code);
+	}
 	else
 		exit(0);
 	return (0);
