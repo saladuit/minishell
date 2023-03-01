8afa0dedc4e8fb53182e07ff03b6cd00202849a5
safoh <safoh@student.codam.nl>
2023-03-01 23:37:51 +0100

    check if my precommit hook works

--- unit_test/src/types_cases.c
+++ unit_test/src/types_cases.c
@@ -8,17 +8,17 @@ bool	is_pipe(int c);
 
 Test(is_pipe, basic)
 {
-  cr_assert(is_pipe('|') == true);
+	cr_assert(is_pipe('|') == true);
 }
 
 Test(is_pipe, upper_boundary)
 {
-  cr_assert(is_pipe('|' + 1) == false);
+	cr_assert(is_pipe('|' + 1) == false);
 }
 
 Test(is_pipe, lower_boundary)
 {
-  cr_assert(is_pipe('|' - 1) == false);
+	cr_assert(is_pipe('|' - 1) == false);
 }
 
 /*******************************************************************************/
@@ -29,17 +29,17 @@ bool	is_dollar(int c);
 
 Test(is_dollar, basic)
 {
-  cr_assert(is_dollar('$') == true);
+	cr_assert(is_dollar('$') == true);
 }
 
 Test(is_dollar, upper_boundary)
 {
-  cr_assert(is_dollar('$' + 1) == false);
+	cr_assert(is_dollar('$' + 1) == false);
 }
 
 Test(is_dollar, lower_boundary)
 {
-  cr_assert(is_dollar('$' - 1) == false);
+	cr_assert(is_dollar('$' - 1) == false);
 }
 
 /*******************************************************************************/
@@ -50,17 +50,17 @@ bool	is_double_quote(int c);
 
 Test(is_double_quote, basic)
 {
-  cr_assert(is_double_quote('\"') == true);
+	cr_assert(is_double_quote('\"') == true);
 }
 
 Test(is_double_quote, upper_boundary)
 {
-  cr_assert(is_double_quote('\"' + 1) == false);
+	cr_assert(is_double_quote('\"' + 1) == false);
 }
 
 Test(is_double_quote, lower_boundary)
 {
-  cr_assert(is_double_quote('\"' - 1) == false);
+	cr_assert(is_double_quote('\"' - 1) == false);
 }
 
 /*******************************************************************************/
@@ -71,32 +71,32 @@ bool	is_redir(int c);
 
 Test(is_redir, input_basic)
 {
-  cr_assert(is_redir('<') == true);
+	cr_assert(is_redir('<') == true);
 }
 
 Test(is_redir, input_upper_boundary)
 {
-  cr_assert(is_redir('<' + 1) == false);
+	cr_assert(is_redir('<' + 1) == false);
 }
 
 Test(is_redir, input_lower_boundary)
 {
-  cr_assert(is_redir('<' - 1) == false);
+	cr_assert(is_redir('<' - 1) == false);
 }
 
 Test(is_redir, output_basic)
 {
-  cr_assert(is_redir('>') == true);
+	cr_assert(is_redir('>') == true);
 }
 
 Test(is_redir, output_upper_boundary)
 {
-  cr_assert(is_redir('>' + 1) == false);
+	cr_assert(is_redir('>' + 1) == false);
 }
 
 Test(is_redir, output_lower_boundary)
 {
-  cr_assert(is_redir('>' - 1) == false);
+	cr_assert(is_redir('>' - 1) == false);
 }
 
 /*******************************************************************************/
@@ -107,15 +107,15 @@ bool	is_single_quote(int c);
 
 Test(is_single_quote, basic)
 {
-  cr_assert(is_single_quote('\'') == true);
+	cr_assert(is_single_quote('\'') == true);
 }
 
 Test(is_single_quote, upper_boundary)
 {
-  cr_assert(is_single_quote('\'' + 1) == false);
+	cr_assert(is_single_quote('\'' + 1) == false);
 }
 
 Test(is_single_quote, lower_boundary)
 {
-  cr_assert(is_single_quote('\'' - 1) == false);
+	cr_assert(is_single_quote('\'' - 1) == false);
 }
