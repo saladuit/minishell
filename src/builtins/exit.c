7032152ae1ee9425fc8cd68004f59b77db658c15
safoh <safoh@student.codam.nl>
2023-03-02 21:28:38 +0100

    Rewrote exit so that it doesn't call exit
    However unsure how to properly handle this as my program should eventually quit after calling this

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,13 +1,13 @@
 #include <minishell.h>
 
-int	ft_exit(char **arguments, t_minishell *shell)
+int ft_exit(char **args, t_minishell *shell)
 {
-	(void)arguments;
-	(void)shell;
-	rl_clear_history();
-	if (arguments[1])
-		exit((unsigned short)ft_atoi(arguments[1]));
-	else
-		exit(0);
-	return (0);
+    int status = EXIT_SUCCESS;
+
+    (void)shell;
+    if (args[1] != NULL) 
+    {
+        status = atoi(args[1]) % 256;
+    }
+    return status;
 }
