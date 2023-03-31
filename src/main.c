260787917515a9cb059e18233710ea2ab353738b
Saladin Afoh <safoh@student.codam.nl>
2023-03-31 14:00:08 +0200

    Fix segfault properly

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int	main(int argc, char **argv, char **envp)
 	if (argc > 1)
 	{
 		write(STDERR_FILENO, (char *)message_lookup(E_USAGE), 7);
-		if (argv[0])
+		if (argv)
 			ft_putendl_fd(*argv, STDERR_FILENO);
 		else
 			ft_putendl_fd("sheldon", STDERR_FILENO);
