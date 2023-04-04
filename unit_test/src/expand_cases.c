e60dd7930a578e090ab9ca417fd9f98e0d6cd4d9
lvan-bus <lucienvanbussel@gmail.com>
2023-04-04 17:26:14 +0200

    added last test cases and everything passes

--- unit_test/src/expand_cases.c
+++ unit_test/src/expand_cases.c
@@ -7,7 +7,6 @@ extern char	**environ;
 void setup_env(void)
 {
     setenv("HELLO", "Hello", 1);
-	setenv("$", "$", 1);
     setenv("WORLD", "World", 1);
     unsetenv("UNSET");
     setenv("SPACE", "Spa ce", 1);
@@ -35,10 +34,39 @@ void assert_expand_token(char *in, char *expected, t_status *status)
     char *input;
     input = ft_strdup(in);
     char *output = expand_token(input, status, env);
-    cr_expect_str_eq(output, expected);
+    cr_expect_str_eq(output, expected, "The expression (as strings) (output) == (expected) is false: actual=`%s` expected=%s` input='%s'",output, expected, in);
     free(output);
     free(input);
 	dict_destroy(env);
+}
+
+/*******************************************************************************/
+/*                           Basic_test_cases                                  */
+/*******************************************************************************/
+
+Test(expand_token, envvar_not_set)
+{
+	assert_expand_token("$UNSET", "", NULL);
+}
+
+//Test(expand_token, null_null, .signal=SIGABRT)
+//{
+//	expand_token(NULL, NULL, NULL);
+//}
+
+Test(expand_token, empty)
+{
+	assert_expand_token("", "", NULL);
+}
+
+Test(expand_token, string)
+{
+	assert_expand_token("Hello", "Hello", NULL);
+}
+
+Test(expand_token, envvar_single)
+{
+	assert_expand_token("$HELLO", "Hello", NULL);
 }
 
 Test(expand_token, exit_status)
@@ -51,14 +79,14 @@ Test(expand_token, exit_status_max)
     assert_expand_token("$?", "255", &max);
 }
 
-Test(expand_token, empty)
+Test(expand_token, echo_shell_in_double_quotes)
 {
-    assert_expand_token("", "", NULL);
+	assert_expand_token("\"$HELLO\"", "Hello", NULL);
 }
 
-Test(expand_token, string)
+Test(expand_token, echo_shell_in_single_quotes)
 {
-    assert_expand_token("Hello", "Hello", NULL);
+	assert_expand_token("\'$HELLO\'", "$HELLO", NULL);
 }
 
 Test(expand_token, envvar_dollar)
@@ -66,40 +94,102 @@ Test(expand_token, envvar_dollar)
     assert_expand_token("$", "$", NULL);
 }
 
- // Test(expand_token, envvar_double_dollar)
- // {
- //     assert_expand_token("$$", "$$", NULL);
- // }
-
-
-// Test(expand_token, envar_dollar_with_space_and_letter)
-// {
-//     assert_expand_token("$ a", "$ a", NULL);
-// }
-
-Test(expand_token, envvar_not_set)
+Test(expand_token, envvar_double_dollar)
 {
-    assert_expand_token("$UNSET", "", NULL);
+	assert_expand_token("$$", "", NULL);
 }
 
-Test(expand_token, envvar_single)
+Test(expand_token, string_twice)
 {
-    assert_expand_token("$HELLO", "Hello", NULL);
+	assert_expand_token("HelloHello", "HelloHello", NULL);
 }
 
-// Test(expand_token, envvar_double)
-// {
-//     assert_expand_token("$HELLO $?", "Hello 255", &max);
-// }
-
-Test(expand_token, envvar_var_combo_1)
+Test(expand_token, envvar_single_twice)
 {
-    assert_expand_token("$HELLO", "Hello", NULL);
+	assert_expand_token("$HELLO$HELLO", "HelloHello", NULL);
 }
 
-Test(expand_token, envvar_var_combo_2)
+Test(expand_token, exit_status_twice)
 {
-    assert_expand_token("$HELLO", "Hello", NULL);
+	assert_expand_token("$?$?", "00", &zero);
+}
+
+Test(expand_token, exit_status_max_twice)
+{
+	assert_expand_token("$?$?", "255255", &max);
+}
+
+Test(expand_token, echo_shell_in_double_quotes_twice)
+{
+	assert_expand_token("\"$HELLO\"\"$HELLO\"", "HelloHello", NULL);
+}
+
+Test(expand_token, echo_shell_in_single_quotes_twice)
+{
+	assert_expand_token("\'$HELLO\'\'$HELLO\'", "$HELLO$HELLO", NULL);
+}
+
+Test(expand_token, envvar_double_dollar_twice)
+{
+	assert_expand_token("$$$$", "", NULL);
+}
+
+Test(expand_token, string_twice_with_space)
+{
+	assert_expand_token("Hello Hello", "Hello Hello", NULL);
+}
+
+Test(expand_token, envvar_single_twice_with_space)
+{
+	assert_expand_token("$HELLO $HELLO", "Hello Hello", NULL);
+}
+
+Test(expand_token, exit_status_twice_with_space)
+{
+	assert_expand_token("$? $?", "0 0", &zero);
+}
+
+Test(expand_token, exit_status_max_twice_with_space)
+{
+	assert_expand_token("$? $?", "255 255", &max);
+}
+
+Test(expand_token, echo_shell_in_double_quotes_twice_with_space)
+{
+	assert_expand_token("\"$HELLO\" \"$HELLO\"", "Hello Hello", NULL);
+}
+
+Test(expand_token, echo_shell_in_single_quotes_twice_with_space)
+{
+	assert_expand_token("\'$HELLO\' \'$HELLO\'", "$HELLO $HELLO", NULL);
+}
+
+Test(expand_token, envvar_dollar_twice_with_space)
+{
+	assert_expand_token("$ $", "$ $", NULL);
+}
+
+Test(expand_token, envvar_double_dollar_twice_with_space)
+{
+	assert_expand_token("$$ $$", " ", NULL);
+}
+
+Test(expand_token, all_basic_cases_together)
+{
+	assert_expand_token("HELLO$HELLO\"$HELLO\"\'$HELLO\'$$$$?", "HELLOHelloHello$HELLO?", &zero);
+}
+
+/*******************************************************************************/
+/*                           Random_test_cases                                 */
+/*******************************************************************************/
+
+Test(expand_token, envar_dollar_with_space_and_letter)
+{
+	assert_expand_token("$ a", "$ a", NULL);
+}
+Test(expand_token, envvar_double)
+{
+	assert_expand_token("$HELLO $?", "Hello 255", &max);
 }
 
 Test(expand_token, envvar_single_quotes)
@@ -107,41 +197,20 @@ Test(expand_token, envvar_single_quotes)
     assert_expand_token("\'$VAR\'", "$VAR", NULL);
 }
 
-// Test(expand_token, echo_shell_shell)
-// {
-// 	assert_expand_token("$SHELL$SHELL", "/bin/zsh/bin/zsh", NULL);
-// }
-
-Test(expand_token, envvar_double_dollar)
-{
-	assert_expand_token("$$", "$", NULL);
-}
-
 Test(expand_token, echo_shell_shell_ja)
 {
-	assert_expand_token("$SHELL$SHELLja", "/bin/zsh", NULL);
+	assert_expand_token("$HELLO$HELLOja", "Hello", NULL);
 }
 
 Test(expand_token, echo_shell_shell_ja_in_single_quotes)
 {
-	assert_expand_token("$SHELL$SHELL\'ja\'", "/bin/zsh/bin/zshja", NULL);
+	assert_expand_token("$HELLO$HELLO\'ja\'", "HelloHelloja", NULL);
 }
 
 Test(expand_token, echo_shell_shell_ja_in_double_quotes)
 {
-	assert_expand_token("$SHELL$SHELL\"ja\"", "/bin/zsh/bin/zshja", NULL);
+	assert_expand_token("$HELLO$HELLO\"ja\"", "HelloHelloja", NULL);
 }
-
-// NEXT 3 FAILING
-// Test(expand_token, echo_shell_shell_ja)
-// {
-// 	assert_expand_token("$SHELL$SHELLja", "/bin/zsh/bin/zshja", NULL);
-// }
-
-// Test(expand_token, echo_shell_shell_double_dollar)
-// {
-// 	assert_expand_token("$SHELL$SHELL$$$$", "/bin/zsh/bin/zsh$$", NULL);
-// }
 
 Test(expand_token, echo_abcdef)
 {
@@ -158,49 +227,30 @@ Test(expand_token, echo_a_and_b)
 	assert_expand_token("a$A$B\"h\"", "ab c de f gh", NULL);
 }
 
-// IS FAILING
-Test(expand_token, echo_many_empty_inputs)
+Test(expand_token, echo_shell_single_and_double_quotes)
 {
-//	assert_expand_token("$EMPTY \"$EMPTY\" a$EMPTYb a\"$EMPTY\"b", " a ab", NULL);
-	assert_expand_token("\"$EMPTY \"$EMPTY\" a$EMPTYb a\"$EMPTY\"b\"", " a ab", NULL);
+	assert_expand_token("\'$HELLO\'\"$HELLO\"",  "$HELLOHello", NULL);
 }
 
-// Test(expand_token, echo_shell_shell_quotes)
-// {
-// 	assert_expand_token("$SHELL$SHELL\'\'", "/bin/zsh/bin/zsh", NULL);
-// }
+Test(expand_token, echo_shell_shell_quotes)
+{
+	assert_expand_token("$HELLO$HELLO\'\'", "HelloHello", NULL);
+}
 
 Test(expand_token, echo_empty_input)
 {
 	assert_expand_token("  $EMPTY", "  ", NULL);
 }
 
-//
-//Test(expand_token, echo_shell_shell_quotes)
-//{
-//	assert_expand_token("$SHELL$SHELL\'\'", "/bin/zsh/bin/zsh", NULL);
-//}
-//
-//Test(expand_token, echo_hallo_in_quotes)
-//{
-//	assert_expand_token("\"hallo\"", "hallo", NULL);
-//}
-
-//Test(expand_token, echo_shell_shell_quatro_dollar)
-//{
-//	assert_expand_token("$SHELL$SHELL''", "/bin/zsh/bin/zsh37793779", NULL);
-//}
-
-// Test(expand_token, envvar_single_quotes_1)
-// {
-//     assert_expand_token("\'$VAR\"\'", "$VAR\"", NULL);
-// }
+Test(expand_token, envvar_single_quotes_1)
+{
+    assert_expand_token("\'$VAR\"\'", "$VAR\"", NULL);
+}
 
 Test(expand_token, envvar_single_quotes_2)
 {
     assert_expand_token("\'$VAR$\'", "$VAR$", NULL);
 }
-
 
 Test(expand_token, double_quote_no_envvar)
 {
@@ -213,32 +263,32 @@ Test(expand_token, double_quote_envvar)
     assert_expand_token("\"$HELLO\"", "Hello", NULL);
 }
 
-// Test(expand_token, double_quote_two_envvar, .timeout=1)
-// {
-//     assert_expand_token("\"$HELLO$WORLD\"", "HelloWorld", NULL);
-// }
+Test(expand_token, double_quote_two_envvar)
+{
+    assert_expand_token("\"$HELLO$WORLD\"", "HelloWorld", NULL);
+}
 
-// Test(expand_token, envvar_with_spaces)
-// {
-//     assert_expand_token("echo $SPACE", "echo Spa ce", NULL);
-// }
+Test(expand_token, envvar_with_spaces)
+{
+    assert_expand_token("echo $SPACE", "echo Spa ce", NULL);
+}
 
-// Test(expand_token, various_1)
-// {
-//     assert_expand_token("\"$? \'$?\' $?\"", "3 \'3\' 0", &zero);
-// }
+Test(expand_token, various_1)
+{
+    assert_expand_token("\"$? \'$?\' $?\"", "0 \'0\' 0", &zero);
+}
 
-// Test(expand_token, various_2)
-// {
-//     assert_expand_token("$?\'$S?\"\'\"$?\"", "3$SHLVL\"3", &zero);
-// }
+Test(expand_token, various_2)
+{
+	assert_expand_token("$?\'$?\"\'\"$?\"", "0$?\"0", &zero);
+}
 
-// Test(expand_token, various_3)
-// {
-//     assert_expand_token("\"$SHLVL\'$SHLVL\'\"$SHLVL\"\"", "3\'3\'3", &zero);
-// }
+Test(expand_token, various_3)
+{
+    assert_expand_token("\"$?\'$?\'\"$?\"\"", "0\'0\'0", &zero);
+}
 
-// Test(expand_token, various_4)
-// {
-//     assert_expand_token("echo $SPACE", "echo Spa ce", NULL);
-// }
+Test(expand_token, various_4)
+{
+    assert_expand_token("echo $SPACE", "echo Spa ce", NULL);
+}
