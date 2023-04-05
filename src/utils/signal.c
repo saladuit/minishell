77643b05bd83abaf59775cb1f6943bbac2b9110f
Saladin Afoh <safoh@student.codam.nl>
2023-04-05 20:13:51 +0200

    Small compilation error fix

--- src/utils/signal.c
+++ src/utils/signal.c
@@ -1,14 +1,14 @@
 #include <minishell.h>
 #include <unistd.h>
 
-void	sigint_handler(int sig)
-{
-	(void)sig;
-	printf("\n");
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
-}
+// void	sigint_handler(int sig)
+// {
+// 	(void)sig;
+// 	printf("\n");
+// 	rl_replace_line("", 0);
+// 	rl_on_new_line();
+// 	rl_redisplay();
+// }
 
 // 30 is an arbitrary number that most likely is not used as a legit exit code.
 // Thus it only ever happens when interupting heredoc.
@@ -38,25 +38,25 @@ void	heredoc_sig_handler(int sig)
 // 	signal(SIGQUIT, SIG_IGN);
 // }
 
-void	setup_signals(t_signal_handler handler)
-{
-	if (handler == SREADLINE)
-	{
-		rl_catch_signals = 0;
-		signal(SIGINT, sigint_handler);
-		signal(SIGQUIT, SIG_IGN);
-	}
-	else if (handler == SHEREDOC)
-	{
-		signal(SIGINT, heredoc_sig_handler);
-		signal(SIGQUIT, SIG_IGN);
-	}
-	else if (handler == SEXECUTOR)
-	{
-		signal(SIGINT, execution_sigint_handler);
-		signal(SIGQUIT, execution_sigquit_handler);
-	}
-}
+// void	setup_signals(t_signal_handler handler)
+// {
+// 	if (handler == SREADLINE)
+// 	{
+// 		rl_catch_signals = 0;
+// 		signal(SIGINT, sigint_handler);
+// 		signal(SIGQUIT, SIG_IGN);
+// 	}
+// 	else if (handler == SHEREDOC)
+// 	{
+// 		signal(SIGINT, heredoc_sig_handler);
+// 		signal(SIGQUIT, SIG_IGN);
+// 	}
+// 	else if (handler == SEXECUTOR)
+// 	{
+// 		signal(SIGINT, execution_sigint_handler);
+// 		signal(SIGQUIT, execution_sigquit_handler);
+// 	}
+// }
 //	struct termios tio;
 //	tcgetattr(STDIN_FILENO, &tio);
 //	tcsetattr(STDIN_FILENO, TCSANOW, &tio);
