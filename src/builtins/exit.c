11326a2190c174c67bed30dcda8cadbec22f5968
lvan-bus <lucienvanbussel@gmail.com>
2023-04-05 17:08:59 +0200

    update exit function

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,14 +1,10 @@
 #include <minishell.h>
 
-int	ft_exit(char **args, t_minishell *shell)
+void	ft_exit(char **args, t_minishell *shell)
 {
-	int	status;
-
-	status = EXIT_SUCCESS;
-	(void)shell;
 	if (args[1] != NULL)
 	{
-		status = atoi(args[1]) % 256;
+		shell->status = ft_atoi(args[1]) % 256;
 	}
-	return (status);
+	shell->stop = true;
 }
