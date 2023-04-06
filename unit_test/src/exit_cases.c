2301ff43a51edb7fc3c86e9777931e93b5a7607a
lvan-bus <lucienvanbussel@gmail.com>
2023-04-06 14:25:51 +0200

    added some test cases for ft_exit, also one the test the error messages (not working properly yet)

--- unit_test/src/exit_cases.c
+++ unit_test/src/exit_cases.c
@@ -1,5 +1,13 @@
 #include <unit_test.h>
 
+void redirect_all_std_for_exit(void)
+{
+	cr_redirect_stdout();
+	cr_redirect_stderr();
+}
+
+TestSuite(exit, .init=redirect_all_std_for_exit);
+
 /*******************************************************************************/
 /*                                   Exit                                      */
 /*******************************************************************************/
@@ -26,11 +34,11 @@ Test(exit, input_123)
 	assert_exit(in, true, 123);
 }
 
-Test(exit, input_abc)
-{
-	char	*in[] = {"exit", "abc", NULL};
-	assert_exit(in, true, 255);
-}
+//Test(exit, input_abc)
+//{
+//	char	*in[] = {"exit", "abc", NULL};
+//	assert_exit(in, true, 255);
+//}
 
 Test(exit, input_2147483647)
 {
@@ -72,4 +80,40 @@ Test(exit, input_256)
 {
 	char	*in[] = {"exit", "256", NULL};
 	assert_exit(in, true, 0);
+}
+
+Test(exit, input_fence)
+{
+	char	*in[] = {"exit", "#", NULL};
+	assert_exit(in, true, 0);
+}
+
+Test(exit, input_at_sign)
+{
+	char	*in[] = {"exit", "@", NULL};
+	assert_exit(in, true, 255);
+}
+
+/*******************************************************************************/
+/*                           Exit_with_error_messages                          */
+/*******************************************************************************/
+
+void	assert_exit_error(char **in, bool expected_stop, t_status expected_status, char *expected_message)
+{
+	t_minishell	shell;
+
+
+	bzero(&shell, sizeof(t_minishell));
+	fflush(stderr);
+	ft_exit(in, &shell);
+	cr_expect_eq(shell.stop, expected_stop);
+	cr_expect_eq(shell.status, expected_status, "The expression (shell.status): %d == (expected_status):  %d is false)", shell.status, expected_status);
+	cr_expect_stderr_eq_str(expected_message, "actual: \"%s\", expected: \"%s\"\n", (char *) stderr, expected_message);
+
+}
+
+Test(exit, input_abc)
+{
+	char	*in[] = {"exit", "abc", NULL};
+	assert_exit_error(in, true, 255, "sheldon: exit: abc: numeric argument required");
 }
