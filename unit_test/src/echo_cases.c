a4b211f84fabc9b3fbb353bee5fb4b0de5ffd021
lvan-bus <lucienvanbussel@gmail.com>
2023-04-14 16:45:35 +0200

    added test cases for ft_echo, passing allleeeeezzzz

--- unit_test/src/echo_cases.c
+++ unit_test/src/echo_cases.c
@@ -1,3 +1,93 @@
-//
-// Created by Lucien Van Bussel on 4/14/23.
-//
+#include <unit_test.h>
+
+/*******************************************************************************/
+/*                          	    Echo_cases                                 */
+/*******************************************************************************/
+
+void	assert_echo_case(char **input, char *expected)
+{
+	t_minishell	shell;
+
+	bzero(&shell, sizeof(shell));
+	ft_echo(input, &shell);
+	cr_assert_stdout_eq_str(expected);
+}
+
+TestSuite(echo, .init=redirect_all_std);
+
+Test(echo, input_empty)
+{
+	char		*input[] = {"echo", NULL};
+	char		*expected = "\n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_empty_with_flag)
+{
+	char		*input[] = {"echo", "-n", NULL};
+	char		*expected = "";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_hello_world)
+{
+	char		*input[] = {"echo", "hello world", NULL};
+	char		*expected = "hello world\n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_two_back_slashes)
+{
+	char		*input[] = {"echo", "\\\\", NULL};
+	char		*expected = "\\\\\n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_one_back_slashes)
+{
+	char		*input[] = {"echo", "\\", NULL};
+	char		*expected = "\\\n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_two_single_quotes_with_space_inside)
+{
+	char		*input[] = {"echo", " ", NULL};
+	char		*expected = " \n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_space)
+{
+	char		*input[] = {"echo", " ", NULL};
+	char		*expected = " \n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_newline)
+{
+	char		*input[] = {"echo", "\n", NULL};
+	char		*expected = "\n\n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_very_long_string)
+{
+	char		*input[] = {"echo", "a very long string with lots of characters and spaces... ", NULL};
+	char		*expected = "a very long string with lots of characters and spaces... \n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_special_chars)
+{
+	char		*input[] = {"echo", "!@#$%^&*()", NULL};
+	char		*expected = "!@#$%^&*()\n";
+	assert_echo_case(input, expected);
+}
+
+Test(echo, input_multiple_args)
+{
+	char		*input[] = {"echo", "multiple", "args", NULL};
+	char		*expected = "multiple args\n";
+	assert_echo_case(input, expected);
+}
