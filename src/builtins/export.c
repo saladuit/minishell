a7cfbc443fd8b35358973ca99463be97f30a1406
lvan-bus <lucienvanbussel@gmail.com>
2023-04-19 14:19:28 +0200

    now also norminette proof

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,73 +1,57 @@
 #include <minishell.h>
 
-static void	export_error_msg(char *arg, bool *not_valid)
+static void	export_error_msg_not_valid(char *arg, t_status *status)
 {
-		ft_putstr_fd(SHELDON, STDERR_FILENO);
-		ft_putstr_fd(": export: `", STDERR_FILENO);
-		ft_putstr_fd(arg, STDERR_FILENO);
-		ft_putendl_fd("': not a valid identifier", STDERR_FILENO);
-		*not_valid = true;
+	ft_putstr_fd(SHELDON, STDERR_FILENO);
+	ft_putstr_fd(": export: `", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putendl_fd("': not a valid identifier", STDERR_FILENO);
+	*status = E_GENERAL;
 }
 
-static void	export_clear(char *key, char *value)
+static void	export_error_msg_out_of_memory(t_minishell *shell, size_t *i)
 {
-	if (key)
-		free(key);
-	if (value)
-		free(value);
+	ft_putstr_fd("export: error: out of memory\n", STDERR_FILENO);
+	shell->status = E_BUILTIN;
+	(*i)++;
+}
+
+static void	get_value(char **value, char *key)
+{
+	*value = ft_strchr(key, '=');
+	if (*value == NULL)
+		*value = ft_strdup("");
+	else
+	{
+		**value = '\0';
+		*value = ft_strdup(++(*value));
+	}
 }
 
 int	ft_export(char **arguments, t_minishell *shell)
 {
-	bool 	not_valid;
 	char	*key;
 	char	*value;
-	int		i;
+	size_t	i;
 
-
-	i = 0;
-	if (!arguments[i++] && !arguments[i])
-	{
-		dict_print(&shell->env);
-		return (SUCCESS);
-	}
+	i = 1;
+	if (!arguments[i])
+		return (dict_print(&shell->env), SUCCESS);
 	while (arguments[i] != NULL)
 	{
 		if (!ft_isalpha(arguments[i][0]))
 		{
-			export_error_msg(arguments[i++], &not_valid);
-			continue;
+			export_error_msg_not_valid(arguments[i++], &shell->status);
+			continue ;
 		}
 		key = ft_strdup(arguments[i]);
 		if (key == NULL)
-		{
-			ft_putstr_fd("export: error: out of memory\n", STDERR_FILENO);
-			shell->status = E_BUILTIN;
-			i++;
-			break ;
-		}
-		value = ft_strchr(key, '=');
-		if (value == NULL)
-			value = ft_strdup("");
-		else
-		{
-			*value = '\0';
-			value = ft_strdup(++value);
-		}
-		if (!value || dict_set(&shell->env, key, value) == ERROR)
-		{
-			ft_putstr_fd("export: error: out of memory\n", STDERR_FILENO);
-			export_clear(key, value);
-			shell->status = E_BUILTIN;
-			i++;
-			break ;
-		}
+			return (export_error_msg_out_of_memory(shell, &i), CONTINUE);
+		get_value(&value, key);
+		if (!value)
+			return (export_error_msg_out_of_memory(shell, &i), CONTINUE);
+		dict_set(&shell->env, key, value);
 		i++;
-	}
-	if (not_valid)
-	{
-		shell->status = E_GENERAL;
-		return (CONTINUE);
 	}
 	return (SUCCESS);
 }
