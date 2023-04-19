5a6fe0e3d6c9dcf7b06d0793ddd44190943a9146
lvan-bus <lucienvanbussel@gmail.com>
2023-04-19 14:21:00 +0200

    did some small fixes, also added malloc checker to unit test

--- unit_test/src/export_cases.c
+++ unit_test/src/export_cases.c
@@ -7,16 +7,16 @@
 void	assert_export_no_arg(char **in, t_status expected_status)
 {
 	t_minishell	shell;
-	bzero(&shell, sizeof(t_minishell));
 
-	ft_export(in, NULL);
+	bzero(&shell, sizeof(t_minishell));
+	ft_export(in, &shell);
 	cr_assert_eq(shell.status, expected_status);
 }
 
 Test(export, no_argument)
 {
 	char	*in[] = {"export", NULL};
-	assert_export_no_arg(in, 0);
+	assert_export_no_arg(in, E_USAGE);
 }
 
 /*******************************************************************************/
@@ -39,67 +39,67 @@ void	assert_export(char **in, char *key, char *expected_value, t_status expected
 	cr_assert_str_eq(expression, expected_value);
 }
 
-Test(export, export_one)
+Test(export, one_arg)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "HELLO=Hello", NULL};
 	char	*key = "HELLO";
 	char	*expected_value = "Hello";
-	assert_export(in, key, expected_value, 0, environ);
+	assert_export(in, key, expected_value, E_USAGE, environ);
 }
 
-Test(export, export_one_equal_sign_and_another)
+Test(export, one_arg_with_equal_sign_in_value)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "HELLO=Hello=bye", NULL};
 	char	*key = "HELLO";
 	char	*expected_value = "Hello=bye";
-	assert_export(in, key, expected_value, 0, environ);
+	assert_export(in, key, expected_value, E_USAGE, environ);
 }
 
-Test(export, export_two_test_one)
+Test(export, two_args)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "ORANGE=Orange" , "BANANAS=Bananas", NULL};
 	char	*key = "ORANGE";
 	char	*expected_value = "Orange";
-	assert_export(in, key, expected_value, 0, environ);
+	assert_export(in, key, expected_value, E_USAGE, environ);
 }
 
-Test(export, export_two_test_two)
+Test(export, two_args_test_the_second_one)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "ORANGE=Orange" , "BANANAS=Bananas", NULL};
 	char	*key = "BANANAS";
 	char	*expected_value = "Bananas";
-	assert_export(in, key, expected_value, 0, environ);
+	assert_export(in, key, expected_value, E_USAGE, environ);
 }
 
-Test(export, export_not_valid_with_valid)
+Test(export, two_args_first_invalid_second_valid)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "1234", "CHECK=check", NULL};
 	char	*key = "CHECK";
 	char	*expected_value = "check";
-	assert_export(in, key, expected_value, 1, environ);
+	assert_export(in, key, expected_value, E_GENERAL, environ);
 }
 
-Test(export, export_one_empty)
+Test(export, empty_value)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "HELLO", NULL};
 	char	*key = "HELLO";
 	char	*expected_value = "";
-	assert_export(in, key, expected_value, 0, environ);
+	assert_export(in, key, expected_value, E_USAGE, environ);
 }
 
-Test(export, export_one_empty_with_equal_sign)
+Test(export, empty_value_with_equal_sign)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "HELLO=", NULL};
 	char	*key = "HELLO";
 	char	*expected_value = "";
-	assert_export(in, key, expected_value, 0, environ);
+	assert_export(in, key, expected_value, E_USAGE, environ);
 }
 
 /*******************************************************************************/
@@ -122,30 +122,84 @@ void	assert_export_not_valid_cases(char **in, t_status expected_status, char **e
 	cr_expect_stderr_eq_str(message);
 }
 
-Test(export, not_a_valid_identifier_one)
+Test(export, invalid_key)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "1234", NULL};
-	assert_export_not_valid_cases(in, 1, environ, "Sheldon: export: `1234': not a valid identifier\n");
+	assert_export_not_valid_cases(in, E_GENERAL, environ, "Sheldon: export: `1234': not a valid identifier\n");
 }
 
-Test(export, not_a_valid_identifier_two)
+Test(export, two_invalid_keys)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "1234", "5678", NULL};
-	assert_export_not_valid_cases(in, 1, environ, "Sheldon: export: `1234': not a valid identifier\nSheldon: export: `5678': not a valid identifier\n");
+	assert_export_not_valid_cases(in, E_GENERAL, environ, "Sheldon: export: `1234': not a valid identifier\nSheldon: export: `5678': not a valid identifier\n");
 }
 
-Test(export, not_a_valid_identifier_with_valid_arg)
+Test(export, invalid_key_and_a_valid_key)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "1234", "check", NULL};
-	assert_export_not_valid_cases(in, 1, environ, "Sheldon: export: `1234': not a valid identifier\n");
+	assert_export_not_valid_cases(in, E_GENERAL, environ, "Sheldon: export: `1234': not a valid identifier\n");
 }
 
-Test(export, not_a_valid_with_equal_sign)
+Test(export, invalid_with_equal_sign)
 {
 	char	*environ[] = {"", NULL};
 	char	*in[] = {"export", "=value", NULL};
-	assert_export_not_valid_cases(in, 1, environ, "Sheldon: export: `=value': not a valid identifier\n");
+	assert_export_not_valid_cases(in, E_GENERAL, environ, "Sheldon: export: `=value': not a valid identifier\n");
+}
+
+/*******************************************************************************/
+/*                           Export_malloc_check                               */
+/*******************************************************************************/
+
+void	assert_export_malloc_check(char **in, t_status expected_status, char **environ)
+{
+	t_minishell	shell;
+	int			condition;
+
+	(void)expected_status;
+	bzero(&shell, sizeof(t_minishell));
+	envp_load(&shell.env, environ);
+	set_malloc_failure_condition(0);
+	ft_export(in, &shell);
+	condition = get_malloc_failure_condition();
+	while (condition > 0)
+	{
+		activate_malloc_hook();
+		set_malloc_failure_condition(condition);
+		ft_export(in, &shell);
+		deactivate_malloc_hook();
+		condition--;
+	}
+	dict_destroy(&shell.env);
+}
+
+Test(export, malloc_check_one_arg)
+{
+	char	*environ[] = {"", NULL};
+	char	*in[] = {"export", "HELLO=Hello", NULL};
+	assert_export_malloc_check(in, E_BUILTIN, environ);
+}
+
+Test(export, malloc_check_two_args)
+{
+	char	*environ[] = {"", NULL};
+	char	*in[] = {"export", "ORANGE=Orange" , "BANANAS=Bananas", NULL};
+	assert_export_malloc_check(in, E_BUILTIN, environ);
+}
+
+Test(export, malloc_check_invalid_arg)
+{
+	char	*environ[] = {"", NULL};
+	char	*in[] = {"export", "1234", NULL};
+	assert_export_malloc_check(in, E_BUILTIN, environ);
+}
+
+Test(export, malloc_check_no_arg)
+{
+	char	*environ[] = {"", NULL};
+	char	*in[] = {"export", NULL};
+	assert_export_malloc_check(in, E_BUILTIN, environ);
 }
