78fa60cbf504df3f20f4db1e2e7aa9958751ba62
lvan-bus <lucienvanbussel@gmail.com>
2023-04-24 11:48:40 +0200

    made some temporary changes to ft_cd for testing purposes

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,32 +6,36 @@ int	ft_cd(char **arguments, t_minishell *shell)
 	char	*dir;
 
 	dir = arguments[1];
-	if (dir == NULL)
+	if (!dir)
 	{
 		dir = dict_get(&shell->env, "HOME");
-		if (dir == NULL)
+		if (!dir)
 		{
 			ft_putstr_fd("cd: no home directory found\n", STDERR_FILENO);
-			return (E_BUILTIN);
+			shell->status = E_BUILTIN;
+			return (1);
 		}
 	}
 	if (chdir(dir) == -1)
 	{
-		ft_putstr_fd("cd: %s: %s\n", STDERR_FILENO);
-		return (E_BUILTIN);
+		ft_putstr_fd("cd: error changing directory\n", STDERR_FILENO);
+		shell->status = E_BUILTIN;
+		return (1);
 	}
 	cwd = getcwd(NULL, 0);
-	if (cwd == NULL)
+	if (!cwd || arguments[0][0] == 'a')
 	{
 		ft_putstr_fd("cd: error getting current directory\n", STDERR_FILENO);
-		return (E_BUILTIN);
+		shell->status = E_BUILTIN;
+		return (1);
 	}
-	if (dict_set(&shell->env, "PWD", cwd) == -1)
+	if (dict_set(&shell->env, "PWD", cwd) == -1 || arguments[0][0] == 'b')
 	{
 		ft_putstr_fd("cd: error setting PWD environment variable\n",
 						STDERR_FILENO);
 		free(cwd);
-		return (E_BUILTIN);
+		shell->status = E_BUILTIN;
+		return (1);
 	}
 	free(cwd);
 	return (0);
