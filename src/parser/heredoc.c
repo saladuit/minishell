908988fba00184b4742a6f92d70b56ac2cec2309
lvan-bus <lucienvanbussel@gmail.com>
2023-05-02 13:32:35 +0200

    added draft for heredoc

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -1,3 +1,39 @@
-//
-// Created by Lucien Van Bussel on 5/2/23.
-//
+#include <minishell.h>
+
+static void	exit_d_check(char *line, bool *exit)
+{
+	if (!line)
+	{
+//		write(STDERR_FILENO, "\n", 1);
+//		remove_split(p->path_list);
+		*exit = true;
+	}
+}
+
+int32_t	here_doc(char *delimiter)
+{
+	int		fd[2];
+	char	*line;
+	bool	exit;
+
+	if (pipe(fd) == -1)
+		return (ERROR);
+	exit = false;
+	while (exit == false)
+	{
+		line = readline("> ");
+		exit_d_check(line, &exit);
+		if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		write(fd[1], line, ft_strlen(line));
+		write(fd[1], "\n", 1);
+		free(line);
+	}
+	close(fd[1]);
+	if (exit)
+		return (free(line), close(fd[0]), ERROR);
+	return (fd[0]);
+}
