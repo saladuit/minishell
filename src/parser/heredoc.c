bad763d57400524210f43e5659ffcf0e26553b25
lvan-bus <lucienvanbussel@gmail.com>
2023-05-03 10:57:36 +0200

    minor changes to heredoc

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -1,26 +1,25 @@
 #include <minishell.h>
 
-static void	exit_d_check(char *line, bool *exit)
-{
-	if (!line)
-		*exit = true;
-}
-
+// check !line later, now resolving a segfault
+// error code CNTRL_D = -2 could be a solution
 int32_t	here_doc(char *delimiter)
 {
 	int		fd[2];
 	char	*line;
-	bool	exit;
 
 	if (!delimiter || ft_strlen(delimiter) == 0)
 		return (ERROR);
 	if (pipe(fd) == -1)
 		return (ERROR);
-	exit = false;
-	while (exit == false)
+	while (1)
 	{
 		line = readline("> ");
-		exit_d_check(line, &exit);
+		if (!line)
+		{
+			close(fd[0]);
+			close(fd[1]);
+			return (ERROR);
+		}
 		if (ft_strncmp(line, delimiter, ft_strlen(delimiter)) == 0)
 		{
 			free(line);
@@ -31,7 +30,5 @@ int32_t	here_doc(char *delimiter)
 		free(line);
 	}
 	close(fd[1]);
-	if (exit)
-		return (free(line), close(fd[0]), ERROR);
 	return (fd[0]);
 }
