1169ac92187b3507ff904720ea711db41e091b1d
lvan-bus <lucienvanbussel@gmail.com>
2023-05-04 18:16:41 +0200

    signal handling is working okay

--- src/utils/signal.c
+++ src/utils/signal.c
@@ -4,31 +4,35 @@ void	signal_ctrl_c(int sig)
 {
 	(void)sig;
 	ft_putstr_fd("\n", 1);
+	signal_error = E_GENERAL;
 	rl_on_new_line();
 	rl_redisplay();
 	signal(SIGQUIT, SIG_IGN);
 }
 
-void	signal_ctrl_heredoc(int sig)
-{
-	(void)sig;
-	ft_putstr_fd("\n", 1);
-	exit(errno);
-}
-
-bool	signal_ctrl_d(char *str, char **env)
+bool	signal_ctrl_d(char *str, char **env, t_status *status)
 {
 	(void)env;
+	(void)status;
 	if (str)
 		return (false);
-	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	signal_error = E_USAGE;
+	*status = E_USAGE;
+	ft_putstr_fd("exit", STDOUT_FILENO);
 	return (true);
 }
 
-void	initialize_signal_handling(void)
+void	initialize_signal_handling(t_status *status)
 {
+	(void)status;
 	signal(SIGINT, signal_ctrl_c);
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGTSTP, SIG_IGN);
-	signal(SIGTERM, signal_ctrl_heredoc);
+}
+
+void	signal_ctrl_c_heredoc(int sig)
+{
+	(void)sig;
+	_exit(E_COMMAND_NOT_FOUND);
+	signal(SIGINT, SIG_IGN);
 }
