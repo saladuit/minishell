25d16745cf1093a43e221b96199f28aea56bd634
Saladin Afoh <safoh@student.codam.nl>
2023-05-04 10:19:41 +0200

    Renamed parser modules

---
+++ src/parser/parse_ast.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+#include <minishell.h>
+
+void	deconstruct_ast(t_list **ast)
+{
+	ft_lstclear(ast, deconstruct_command_table);
+	*ast = NULL;
+}
+
+void	debug_ast(t_list *ast)
+{
+	ft_putendl_fd("", 1);
+	print_command_tables(ast);
+}
+
+t_list	*construct_ast(t_list *tokens, t_status *status, t_dictionary *env)
+{
+	t_command_table	*command_table;
+	t_list			*ast;
+
+	ast = NULL;
+	while (tokens)
+	{
+		command_table = construct_command_table(&tokens, status, env);
+		if (!command_table || !ft_lstadd_backnew(&ast, command_table))
+		{
+			deconstruct_command_table(command_table);
+			deconstruct_ast(&ast);
+			return (NULL);
+		}
+	}
+	return (ast);
+}
