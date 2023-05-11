7f8794cf695143df1c50e0b4bfcaa75c357bcb54
Saladin Afoh <safoh@student.codam.nl>
2023-05-11 16:42:59 +0200

    fixed export

---
+++ src/builtins/builtin_export_helpers.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   builtin_export_error_message.c                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lvan-bus <lvan-bus@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/05 09:24:08 by lvan-bus      #+#    #+#                 */
+/*   Updated: 2023/05/05 09:24:09 by lvan-bus      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	export_error_msg_not_valid(char *arg, t_status *status)
+{
+	ft_putstr_fd(SHELDON, STDERR_FILENO);
+	ft_putstr_fd(": export: `", STDERR_FILENO);
+	ft_putstr_fd(arg, STDERR_FILENO);
+	ft_putendl_fd("': not a valid identifier", STDERR_FILENO);
+	*status = E_GENERAL;
+}
+
+void	free_key_value(char **key, char **value)
+{
+	if (key && *key)
+	{
+		free(*key);
+		*key = NULL;
+	}
+	if (value && *value)
+	{
+		free(*value);
+		*value = NULL;
+	}
+}
