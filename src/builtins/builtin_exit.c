7f36cd79658e99e1033e6145e857ffa0998b8d4a
lvan-bus <lucienvanbussel@gmail.com>
2023-05-12 10:33:55 +0200

    rewrote the ft_exit, almost working perfectly

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -12,28 +12,6 @@
 
 #include <minishell.h>
 
-static int	numeric(char *arg)
-{
-	size_t	i;
-
-	i = 0;
-	while (arg[i] && ft_iswhitespace(arg[i]))
-		i++;
-	if (arg[i] == '+' || arg[i] == '-')
-		i++;
-	if (arg[i] == '\0' || arg[i] == '-')
-		return (0);
-	while (arg[i] && arg[i] == '0')
-		i++;
-	while (arg[i] && arg[i] != '#' && arg[i] != ';')
-	{
-		if (!ft_isdigit(arg[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 static void	exit_err_msg(char *arg)
 {
 	ft_putstr_fd(SHELDON, STDERR_FILENO);
@@ -47,7 +25,29 @@ static void	exit_err_msg(char *arg)
 	}
 }
 
-static size_t	count_args(char **args)
+static bool	numeric(char *arg)
+{
+	size_t	i;
+
+	i = 0;
+	while (arg[i] && ft_iswhitespace(arg[i]))
+		i++;
+	if (arg[i] == '-' || arg[i] == '+')
+		i++;
+	if (!arg[i])
+		return (false);
+	while (arg[i] && arg[i] == '0')
+		i++;
+	while (arg[i] && arg[i] != '#' && arg[i] != ';')
+	{
+		if (!ft_isdigit(arg[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static size_t	ft_array_len(char **args)
 {
 	size_t	count;
 
@@ -57,58 +57,56 @@ static size_t	count_args(char **args)
 	return (count);
 }
 
-static t_status	check_edge_cases(char **args)
+static void	handle_count_is_bigger_then_two(char **args, t_status *status, bool *stop)
 {
-	if ((ft_strncmp(args[1], "--", 3) == 0 && ft_strncmp(args[2], "--", 3) == 0)
-			|| ft_strncmp(args[1], "-", 2) == 0 || ft_strncmp(args[1], "+", 2) == 0)
-		return (E_UNKNOWN);
-	if (ft_strncmp(args[1], "--", 3) == 0)
-		return (E_USAGE);
-	if (ft_strncmp(args[1], "?", 2) == 0)
-		return (E_GENERAL);
-	return (E_NO_STATUS);
+	if (numeric(args[1]) == true || ft_strncmp(args[1], "?", 2) == 0)
+	{
+		*status = E_GENERAL;
+		exit_err_msg(NULL);
+		*stop = false;
+	}
+	else
+	{
+		*status = E_UNKNOWN;
+		exit_err_msg(args[1]);
+		*stop = true;
+	}
+}
+
+static void	handle_count_equals_two(char **args, t_status *status)
+{
+	long long	res;
+
+	if (ft_strlen(args[1]) == 0 || !ft_strncmp(args[1], "--", 3))
+		*status = E_USAGE;
+	else if (ft_strncmp(args[1], "?", 2) == 0)
+		*status = E_GENERAL;
+	else if (!numeric(args[1]) || (!ft_ltoi_with_overflow(args[1], &res)))
+	{
+		*status = E_UNKNOWN;
+		exit_err_msg(args[1]);
+	}
+	else
+	{
+		*status = (t_status) ft_atoi(args[1]) % 256;
+		if (*status < 0)
+			*status += 256;
+	}
 }
 
 void	ft_exit(char **args, t_minishell *shell)
 {
-	long long	res;
 	size_t		count;
-	t_status	status;
+	bool		stop;
 
-	count = count_args(args);
-	if (count > 1 && check_edge_cases(args) != E_NO_STATUS)
-	{
-		status = check_edge_cases(args);
-		if (status != E_NO_STATUS)
-			shell->status = status;
-	}
-	else if (count == 2)
-	{
-		if (args[1][0] == '$')
-			shell->status = E_UNKNOWN;
-		else if (ft_strlen(args[1]) == 0)
-			shell->status = E_GENERAL;
-		else if (!numeric(args[1]) || (!ft_ltoi_with_overflow(args[1], &res)))
-		{
-			shell->status = E_UNKNOWN;
-			exit_err_msg(args[1]);
-		}
-		else
-			shell->status = (t_status)ft_atoi(args[1]) % 256;
-	}
-	else if (count > 2 && ft_strbapi(args[1], (bool (*)(int)) &ft_isdigit))
-	{
-		shell->status = E_GENERAL;
-		exit_err_msg(NULL);
-		return ;
-	}
-	else if (count > 2)
-	{
-		shell->status = E_UNKNOWN;
-		exit_err_msg(args[1]);
-	}
+	stop = true;
+	count = ft_array_len(args);
+	if (count > 2)
+		handle_count_is_bigger_then_two(args, &shell->status, &stop);
+	if (count == 2 && stop == true)
+		handle_count_equals_two(args, &shell->status);
 	if (shell->is_pipeline)
 		_exit(shell->status);
-	shell->stop = true;
-	return ;
+	if (stop == true)
+		shell->stop = true;
 }
