094be1513150b0b448ad6d5ea631e920ac578d25
safoh <safoh@student.codam.nl>
2023-05-13 13:23:11 +0200

    Spaces and tabs

--- makerc/common.mk
+++ makerc/common.mk
@@ -13,48 +13,48 @@
 #	common.mk
 
 #	Compiler
-CC											:=gcc
+CC							:=gcc
 
 #	Remove	command
-RM											:=rm -rf
+RM							:=rm -rf
 
 #	Compiler	flags
-CFLAGS									=-Wall -Wextra -Werror
+CFLAGS						=-Wall -Wextra -Werror
 
 #	Directories
-SRC_DIR									:=src
-SRCS_DIRS								:=minishell lexer parser expander executor \
-													utils env types builtins redirections
-INCLUDE_DIR							:=include
-BUILD_DIR								:=build
-LIB_DIR									:=libs
-LIBFT_DIR								:=$(LIB_DIR)/libft
-READLINE_DIR						:=$(LIB_DIR)/readline
-LIBFT_INCLUDE_DIR				:=$(LIBFT_DIR)/$(INCLUDE_DIR)
+SRC_DIR						:=src
+SRCS_DIRS					:=minishell lexer parser expander executor \
+							  utils env types builtins redirections
+INCLUDE_DIR					:=include
+BUILD_DIR					:=build
+LIB_DIR						:=libs
+LIBFT_DIR					:=$(LIB_DIR)/libft
+READLINE_DIR				:=$(LIB_DIR)/readline
+LIBFT_INCLUDE_DIR			:=$(LIBFT_DIR)/$(INCLUDE_DIR)
 
 #	Unit	test	Directories
-UNIT_DIR								:=unit_test
-UNIT_BUILD_DIR					:=$(UNIT_DIR)/$(BUILD_DIR)
-UNIT_SRC_DIR						:=$(UNIT_DIR)/$(SRC_DIR)
-UNIT_INCLUDE_DIR				:=$(UNIT_DIR)/$(INCLUDE_DIR)
+UNIT_DIR					:=unit_test
+UNIT_BUILD_DIR				:=$(UNIT_DIR)/$(BUILD_DIR)
+UNIT_SRC_DIR				:=$(UNIT_DIR)/$(SRC_DIR)
+UNIT_INCLUDE_DIR			:=$(UNIT_DIR)/$(INCLUDE_DIR)
 
 #	Flags
 ifdef	DEBUG
-	CFLAGS								+=-g -MMD -MP
+	CFLAGS					+=-g -MMD -MP
 endif
 
 ifdef LOG
-	CFLAGS								+=-D LOG=1
+	CFLAGS					+=-D LOG=1
 endif
 
 ifdef	COV
-	CFLAGS								+=--coverage
+	CFLAGS					+=--coverage
 endif
 
 ifdef	FSAN
-	CFLAGS								+=-fsanitize=address,undefined
+	CFLAGS					+=-fsanitize=address,undefined
 endif
 
 ifdef	FILTER
-	F											:=--filter $(FILTER)
+	F						:=--filter $(FILTER)
 endif
