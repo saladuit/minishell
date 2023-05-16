ebb928ef5ee34013289c8cb61c8fcfc46df240f5
Saladin Afoh <safoh@student.codam.nl>
2023-05-16 17:15:09 +0200

    Fix

--- src/executor/redirect.c
+++ src/executor/redirect.c
@@ -12,14 +12,6 @@
 
 #include <minishell.h>
 
-static int32_t	redirect_file_descriptor(int source_fd, t_type redirection)
-{
-	if (redirection == INPUT || redirection == HEREDOC)
-		return (dup2(source_fd, STDIN_FILENO));
-	else
-		return (dup2(source_fd, STDOUT_FILENO));
-}
-
 static int32_t	redirect(t_redir *redir, t_type type)
 {
 	int32_t	fd;
@@ -61,6 +53,18 @@ static bool	handle_redirection(t_redir *redir, int32_t *input_fd,
 	return (true);
 }
 
+int32_t redirect_fds(int32_t input_fd, int32_t output_fd)
+{
+	int32_t	status;
+
+	status = SUCCESS;
+	if (input_fd != ERROR)
+		status = dup2(input_fd, STDIN_FILENO);
+	if (status != ERROR && output_fd != ERROR)
+		status = dup2(output_fd, STDOUT_FILENO);
+	return (status);
+}
+
 int32_t	setup_redirects(t_command *command)
 {
 	t_redir	*redir;
@@ -82,5 +86,7 @@ int32_t	setup_redirects(t_command *command)
 		}
 		i++;
 	}
+	if (redirect_fds(input_fd, output_fd) == ERROR)
+		return (E_GENERAL);
 	return (SUCCESS);
 }
